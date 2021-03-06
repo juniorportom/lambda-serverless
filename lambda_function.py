938fa42333cb9be6cf17fb2177024ea43494deb7
Reinaldo Portocarrero <juniorportom@gmail.com>
2021-03-06 14:56:12 -0500

    update code comment of lambda

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import boto3
 import os
 from boto3.dynamodb.conditions import Key
 
-# Hola a todos companeros, Carlos es el mejor profe de AWS del Mundo
+# This is a comment of code in lambda function
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
